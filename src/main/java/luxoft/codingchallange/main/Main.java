49a7abc3c7cf9ead7aeb5105a99f5c60b4fb7972
M.Cevheri BOZOGLAN <cevheribozoglan@gmail.com>
2022-08-12 14:41:06 +0300

    first commit

---
+++ src/main/java/luxoft/codingchallange/main/Main.java
@@ -0,0 +1,21 @@
+package luxoft.codingchallange.main;
+
+import luxoft.codingchallange.filesystem.File;
+import luxoft.codingchallange.filesystem.FileSystem;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO search for files
+        //TODO print list of files found for previous search
+
+
+    }
+
+
+
+
+
+}
