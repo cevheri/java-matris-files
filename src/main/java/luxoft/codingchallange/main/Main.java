e9811b0d5ca55262f7deb0c86ffbb0dd638a9a7d
M.Cevheri BOZOGLAN <cevheribozoglan@gmail.com>
2022-08-12 16:06:57 +0300

    * Implement a way to search in your file system based on any combination of attributes    * eg: all files that have Permission W and created before 18-05-2022    * eg2: all files with name containg "asd"

--- src/main/java/luxoft/codingchallange/main/Main.java
+++ src/main/java/luxoft/codingchallange/main/Main.java
@@ -2,8 +2,13 @@ package luxoft.codingchallange.main;
 
 import luxoft.codingchallange.filesystem.File;
 import luxoft.codingchallange.filesystem.FileSystem;
+import luxoft.codingchallange.filesystem.Permissions;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -11,11 +16,41 @@ public class Main {
         //TODO search for files
         //TODO print list of files found for previous search
 
-
+        searchFilesOldWay();
+        searchFilesStream();
     }
 
 
+    public static void searchFilesOldWay() {
+        for (File[] fileArray : FileSystem.files) {
+            System.out.println("---old way ---");
+            System.out.println(Arrays.toString(fileArray));
 
+            for (File file : fileArray) {
+                if (Permissions.W.equals(file.getPermission())
+                        && LocalDate.of(2022, 05, 18)
+                        .isBefore(ChronoLocalDate.from(file.getTimeCreated())))
+
+                    System.out.println(file.getName());
+            }
+        }
+
+        System.out.println("--- old way ended ---");
+    }
+
+    public static void searchFilesStream() {
+
+        System.out.println("--- using stream ---");
+        Arrays.stream(FileSystem.files)
+                .flatMap(Arrays::stream)
+                .filter(file -> Permissions.W.equals(file.getPermission())
+                        && LocalDate.of(2022, 05, 18).isBefore(ChronoLocalDate.from(file.getTimeCreated()))
+                ).map(File::getName)
+                .filter(f-> f.equals("file40"))
+                .collect(Collectors.toList())
+                .forEach(System.out::println)
+        ;
+    }
 
 
 }
